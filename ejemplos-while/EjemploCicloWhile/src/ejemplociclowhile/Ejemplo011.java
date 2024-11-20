447ea178277f59b1146c180142cc08c6e3ad5f2c
Pablo Ordóñez <pablosebas61@gmail.com>
2024-11-20 11:39:44 -0500

    Cambios while

---
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo011.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplociclowhile;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo011 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        
+        int limite = 13;
+        int contador = 1;
+        
+        while(contador<=limite){
+           System.out.printf("%d\n", contador);
+           contador  = contador  +1;
+        }
+        
+            
+    
+        
+    }
+    
+}
